afc069df644ff3a95f4b204bf1d3fd2f7518aa1d
Hugo MAZERAT <hugo.mazerat@edu.ece.fr>
2023-05-30 16:37:07 +0200

    pbm lien entre php et html

--- traitementVous.php
+++ traitementVous.php
@@ -1,10 +1,72 @@
-<?php
-    session_start();
-    //Par la suite, on doit récupérer les id de l'utilisateur qui est en train d'utiliser le site
-    //Une fois cette récupération effectuée, on doit enregistrer les différentes modifications effectuées dans la base de données
-    $dompdf = new Dompdf();
-    $dompdf->loadHtml($html);
-    $dompdf->render();
-    $dompdf->stream();
+<?php//Probleme de lien entre le html et le php
+
+    //données du formulaire
+    $nom = isset($_POST["nom"])? $_POST["nom"] : "";
+    $prenom = isset($_POST["prenom"])? $_POST["prenom"] : "";
+    $photo = isset($_POST["photo"])? $_POST["photo"] : "";
+    $datenaissance = isset($_POST["datenaissance"])? $_POST["datenaissance"] : "";
+    $mail = isset($_POST["mail"])? $_POST["mail"] : "";
+    $tel = isset($_POST["tel"])? $_POST["tel"] : "";
+    $bio = isset($_POST["bio"])? $_POST["bio"] : "";
+    $experience = isset($_POST["experience"])? $_POST["experience"] : "";
+    //identification BDD
+    $database = "linkedin"; 
+
+    //Connection dans la BDD
+    $db_handle = mysqli_connect('localhost','root','72srmxqo');
+    $db_found = mysqli_select_db($db_handle, $database); 
+
+
+
+    //$linkedin = isset($_POST["personne"])? $_POST["personne"]: "";
+    /*if (empty($linkedin)) {
+        $linkedin=0;
+    }*/
+    
+    //$sql = "";
+    echo "<p>". "Test" . "</p>";
+    //Si la BDD existe
+    if (isset($_POST["save"])) {//Ici le pbm !!!!!!!!!!
+        echo "<p>"."Test2"."</p>";
+        if ($db_found) {
+            //code MySQL. $sql est basé sur le choix de l’utilisateur
+            $sql = "INSERT INTO personne(Nom, Prenom, Photo, DateNaissance, Email, Tel, Bio, Projets) VALUES('$nom', '$prenom', '$photo', '$datenaissance', '$mail', '$tel', '$bio', '$experience')";
+            $result = mysqli_query($db_handle, $sql);
+            echo "<p>Add successful.</p>";
+            //on affiche les données entrées
+            $sql = "SELECT * FROM personne";
+            $result = mysqli_query($db_handle, $sql);
+            echo "<h2>" . "Les données ajoutées sont:" . "</h2>";
+            echo "<table>";
+            echo "<tr>";
+            echo "<th>" . "Votre nom" . "</th>";
+            echo "<th>" . "Votre prénom" . "</th>";
+            echo "<th>" . "Votre photo" . "</th>";
+            echo "<th>" . "Votre date de naissance" . "</th>";
+            echo "<th>" . "Votre e-mail" . "</th>";
+            echo "<th>" . "Votre numéro de téléphone" . "</th>";
+            echo "<th>" . "Votre parcours" . "</th>";
+            echo "<th>" . "Votre expérience la plus pertinente" . "</th>";
+            //afficher le resultat
+            while ($data = mysqli_fetch_assoc($result)) {
+                echo "<tr>";
+                echo "<td>" . $data['nom'] . "</td>";
+                echo "<td>" . $data['prenom'] . "</td>";
+                $image = $data['Couverture'];
+                echo "<td>" . "<img src='$image' height='120' width='100'>" . "</td>";
+                echo "<td>" . $data['datenaissance'] . "</td>";
+                echo "<td>" . $data['mail'] . "</td>";
+                echo "<td>" . $data['tel'] . "</td>";
+                echo "<td>" . $data['bio'] . "</td>";
+                echo "<td>" . $data['experience'] . "</td>";
+                
+                echo "</tr>";
+            }
+            echo "</table>";
+                
+        } else {
+            echo "<p>Database not found.</p>";
+        }
+    }
 ?>
 
