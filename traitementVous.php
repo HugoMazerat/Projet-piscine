f282e0c7244bdc07698c8632f64c35efbbf0c1a5
Hugo MAZERAT <hugo.mazerat@edu.ece.fr>
2023-05-30 12:45:23 +0200

    maj jiji + creation traitement

---
+++ traitementVous.php
@@ -0,0 +1,6 @@
+<?php
+    session_start();
+    //Par la suite, on doit récupérer les id de l'utilisateur qui est en train d'utiliser le site
+    //Une fois cette récupération effectuée, on doit enregistrer les différentes modifications effectuées dans la base de données
+?>
+
