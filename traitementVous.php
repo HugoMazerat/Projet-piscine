c73055f51a636122f9dda60267de25bcad8345b3
Hugo MAZERAT <hugo.mazerat@edu.ece.fr>
2023-05-30 14:56:36 +0200

    maj

--- traitementVous.php
+++ traitementVous.php
@@ -2,5 +2,9 @@
     session_start();
     //Par la suite, on doit récupérer les id de l'utilisateur qui est en train d'utiliser le site
     //Une fois cette récupération effectuée, on doit enregistrer les différentes modifications effectuées dans la base de données
+    $dompdf = new Dompdf();
+    $dompdf->loadHtml($html);
+    $dompdf->render();
+    $dompdf->stream();
 ?>
 
