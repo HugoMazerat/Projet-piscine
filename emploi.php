5b1a130769d8fe68ca26186351873c90b5cf3f3e
GabrielSouquieres <91546353+GabrielSouquieres@users.noreply.github.com>
2023-06-01 13:50:50 +0200

    gngngng hugo veut pas push up

---
+++ emploi.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Alerte Emploi</title>
+</head>
+<body>
+    <h1>Alerte Emploi</h1>
+    
+        <h2 class="Nom">Nom de l'emploi :</h2>
+        <input type="text" name="Nom" id="Nom" required>
+        <br>
+        <h2 class="Type">Type de contrat :</h2>
+        <input type="text" name="Type" id="Type">
+        <br>
+        <h2 class="Salaire">Salaires :</h2>
+        <input type="text" name="Salaire" id="Salaire">
+        <br>
+        <br>
+        <h2 class="Entreprise">Entreprise d'accueil :</h2>
+        <input type="text" name="entreprise" id="Entreprise">
+        <br>
+        <input type="submit" value="Soumettre ma candidature">
+    
+</body>
+</html>
