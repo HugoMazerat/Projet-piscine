aa3d578ee9a404fc5e1f8a5ce3d292cacd13ca97
AntoineJouvenotDiehl <antoine.jouvenotdiehl@edu.ece.fr>
2023-06-01 09:37:41 +0200

    demande amis finie

--- monReseau.php
+++ monReseau.php
@@ -43,34 +43,140 @@ $bdd = new PDO('mysql:host=localhost;dbname=pj1;charset=utf8', 'root', '');
         <a href="#Emplois">Emplois</a>
     </nav>
 
-    <!-- Partie centrale -->
-    <section id="communication-container">
-        <div id="contacts">
-            <div id="contacts-header">
-                <h2>Les utilisateurs d'EceIn</h2>
-            </div>
-            <div id="contacts-list">
-                <?php
-                $recupUser = $bdd->prepare('SELECT * FROM utilisateurs WHERE ID != ?');
-                $recupUser->execute(array($_SESSION['ID']));
-                while ($user = $recupUser->fetch()) {
-                ?>
-                        <p><?php echo $user['nom'];
-                            echo " ";
-                            echo $user['prenom'];
-                            echo " : ";
-                            echo $user['email']; ?></p>
-                    </a>
-                <?php
-                }
-                ?>
-            </div>
-            <div id="select-user">
-                <p>Sélectionnez l'utilisateur à qui vous voulez envoyer une demande d'amie</p>
-            </div>
-        </div>
 
-    </section>
+
+
+
+
+
+
+
+
+
+    <?php
+
+
+// Envoi d'une demande d'amitié
+if(isset($_POST['envoyer_demande'])) {
+    $auteur_demande = $_SESSION['ID'];
+    $destinataire_demande = $_POST['destinataire'];
+    
+    // Vérifier si une demande d'amitié existe déjà entre les deux utilisateurs
+    $checkDemandeExist = $bdd->prepare('SELECT * FROM demande_amis WHERE (id_auteur = ? AND id_destinataire = ?) OR (id_auteur = ? AND id_destinataire = ?)');
+    $checkDemandeExist->execute(array($auteur_demande, $destinataire_demande, $destinataire_demande, $auteur_demande));
+    
+    if($checkDemandeExist->rowCount() == 0) {
+        // Insérer la demande d'amitié dans la base de données
+        $insertDemande = $bdd->prepare('INSERT INTO demande_amis (id_auteur, id_destinataire, status) VALUES (?, ?, "en_attente")');
+        $insertDemande->execute(array($auteur_demande, $destinataire_demande));
+        
+        echo "La demande d'amitié a été envoyée avec succès.";
+    } else {
+        echo "Une demande d'amitié entre ces deux utilisateurs existe déjà.";
+    }
+}
+
+// Accepter une demande d'amitié
+if (isset($_POST['accepter_demande'])) {
+    $id_demande = $_POST['id_demande'];
+
+    // Mettre à jour le statut de la demande d'amitié pour l'accepter
+    $updateDemande = $bdd->prepare('UPDATE demande_amis SET status = "accepté" WHERE id = ?');
+    $updateDemande->execute(array($id_demande));
+
+    // Récupérer les ID de l'auteur de la demande et du destinataire
+    $recupIDs = $bdd->prepare('SELECT id_auteur, id_destinataire FROM demande_amis WHERE id = ?');
+    $recupIDs->execute(array($id_demande));
+    $ids = $recupIDs->fetch();
+
+    $auteur_demande = $ids['id_auteur'];
+    $destinataire_demande = $ids['id_destinataire'];
+
+    // Insérer une nouvelle entrée dans la table "amis"
+    $insertAmi = $bdd->prepare('INSERT INTO amis (id_u1, id_u2) VALUES (?, ?)');
+    $insertAmi->execute(array($auteur_demande, $destinataire_demande));
+
+    echo "La demande d'amitié a été acceptée.";
+}
+
+
+
+// Récupérer la liste des utilisateurs, à l'exception de l'utilisateur actuel
+$recupUser = $bdd->prepare('SELECT * FROM utilisateurs WHERE ID != ?');
+$recupUser->execute(array($_SESSION['ID']));
+?>
+
+<!-- Partie centrale -->
+<section id="communication-container">
+    <div id="contacts">
+        <div id="contacts-header">
+            <h2>Les utilisateurs d'EceIn</h2>
+        </div>
+        <div id="contacts-list">
+            <?php while ($user = $recupUser->fetch()) { ?>
+                <p>
+                    <?php echo $user['nom']; echo " "; echo $user['prenom']; echo " : "; echo $user['email']; ?>
+                    <!-- Formulaire pour envoyer une demande d'amitié -->
+                    <form method="post" action="">
+                        <input type="hidden" name="destinataire" value="<?php echo $user['ID']; ?>">
+                        <button type="submit" name="envoyer_demande">Envoyer demande d'amitié</button>
+                    </form>
+                </p>
+            <?php } ?>
+        </div>
+        <div id="select-user">
+            <p>Sélectionnez l'utilisateur à qui vous voulez envoyer une demande d'amitié</p>
+        </div>
+    </div>
+</section>
+
+<!-- Section pour afficher les demandes d'amitié reçues -->
+<section id="demandes-amis">
+    <h2>Demandes d'amitié reçues</h2>
+    <?php
+    // Récupérer les demandes d'amitié reçues par l'utilisateur actuel
+    $recupDemandes = $bdd->prepare('SELECT demande_amis.*, utilisateurs.nom, utilisateurs.prenom FROM demande_amis INNER JOIN utilisateurs ON demande_amis.id_auteur = utilisateurs.ID WHERE id_destinataire = ? AND status = "en_attente"');
+    $recupDemandes->execute(array($_SESSION['ID']));
+    
+    while ($demande = $recupDemandes->fetch()) {
+        ?>
+        <div class="demande">
+            <p><?php echo $demande['nom']; echo " "; echo $demande['prenom']; ?> souhaite devenir votre ami(e).</p>
+            <!-- Formulaire pour accepter la demande d'amitié -->
+            <form method="post" action="">
+                <input type="hidden" name="id_demande" value="<?php echo $demande['id']; ?>">
+                <button type="submit" name="accepter_demande">Accepter la demande</button>
+            </form>
+        </div>
+        <?php
+    }
+    ?>
+</section>
+
+
+
+<!-- Section pour afficher la liste des amis -->
+<section id="liste-amis">
+    <h2>Mes amis</h2>
+    <?php
+    // Récupérer la liste des amis de l'utilisateur actuel
+    $recupAmis = $bdd->prepare('SELECT utilisateurs.nom, utilisateurs.prenom FROM utilisateurs INNER JOIN amis ON utilisateurs.ID = amis.id_u1 OR utilisateurs.ID = amis.id_u2 WHERE (amis.id_u1 = ? OR amis.id_u2 = ?) AND utilisateurs.ID != ?');
+    $recupAmis->execute(array($_SESSION['ID'], $_SESSION['ID'], $_SESSION['ID']));
+    
+    while ($ami = $recupAmis->fetch()) {
+        ?>
+        <p><?php echo $ami['nom']; echo " "; echo $ami['prenom']; ?></p>
+        <?php
+    }
+    ?>
+</section>
+
+
+
+
+
+
+
 
 
     <!-- Footer -->
