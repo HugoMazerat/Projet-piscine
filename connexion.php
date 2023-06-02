872d30612c52edde3353f47e9e35b14e7935cd36
AntoineJouvenotDiehl <antoine.jouvenotdiehl@edu.ece.fr>
2023-06-02 11:55:13 +0200

    connexion utilisateurs mdp/ rajouter option admi

--- connexion.php
+++ connexion.php
@@ -1,21 +1,32 @@
-<?php 
+<?php
 session_start();
-$bdd = new PDO('mysql:host=localhost; dbname=linkedin; charset=utf8;', 'root', ''); // Connexion à la base de données
-if(isset ($_POST ['valider'])){
-    if(!empty($_POST['email'])){
+$bdd = new PDO('mysql:host=localhost;dbname=linkedin;charset=utf8', 'root', ''); // Connexion à la base de données
+
+if(isset($_POST['valider'])) {
+    if(!empty($_POST['email']) && !empty($_POST['password'])) {
+        $email = $_POST['email'];
+        $password = $_POST['password'];
+
         $recupUser = $bdd->prepare('SELECT * FROM utilisateurs WHERE email = ?');
-        $recupUser->execute(array($_POST['email']));
+        $recupUser->execute(array($email));
 
-        if($recupUser->rowCount() > 0){
+        if($recupUser->rowCount() > 0) {
+            $user = $recupUser->fetch();
 
-            $_SESSION['email'] = $_POST['email'];
-            $_SESSION['ID'] = $recupUser->fetch()['ID'];
-            header('Location: accueil.html');
-        }else{
+            // Vérification du mot de passe
+            if($password === $user['password']) { // Remplacez 'mdp' par le nom réel de la colonne contenant le mot de passe dans votre base de données
+                $_SESSION['email'] = $email;
+                $_SESSION['ID'] = $user['ID'];
+                header('Location: accueil.html');
+                exit;
+            } else {
+                echo "Mot de passe incorrect";
+            }
+        } else {
             echo "Aucun utilisateur trouvé";
         }
-    }else{
-        echo "Veuillez rentrer votre email";
+    } else {
+        echo "Veuillez remplir tous les champs";
     }
 }
 ?>
@@ -32,14 +43,12 @@ if(isset ($_POST ['valider'])){
         <section>
             <h1>Connexion</h1>
             <form method="POST" action="" align="center">
-            <input type="text" name="email">
-            <br/>
-            <input type="submit" name="valider">
-        </form>
+                <input type="text" name="email" placeholder="Email">
+                <br/>
+                <input type="password" name="password" placeholder="Mot de passe">
+                <br/>
+                <input type="submit" name="valider">
+            </form>
         </section>
-       
     </body>
 </html>
-
-
-
